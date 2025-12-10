8d0b025428411c44f4d64b4d032f228de4d7f89b
Vicky Spandonari <149634185+vickyspandonari@users.noreply.github.com>
2025-12-10 00:34:31 -0300

    Fix headless add-to-cart click

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -3,18 +3,23 @@ from .base_page import BasePage
 
 class InventoryPage(BasePage):
 
-    FIRST_PRODUCT_ADD_BTN = (By.CSS_SELECTOR, ".inventory_item:first-child button")
+    FIRST_PRODUCT_ADD_BTN = (By.ID, "add-to-cart-sauce-labs-backpack")
     CART_ICON = (By.ID, "shopping_cart_container")
     ITEM_NAME = (By.CSS_SELECTOR, ".inventory_item_name")
 
     def add_first_product_to_cart(self):
-        # Scroll para evitar errores en headless
+        # Aseguramos que el botón exista
         element = self.find(*self.FIRST_PRODUCT_ADD_BTN)
+
+        # Scroll para evitar fallos en headless
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
-        element.click()
+
+        # Click robusto con WebDriverWait
+        self.click(*self.FIRST_PRODUCT_ADD_BTN)
 
     def go_to_cart(self):
         self.click(*self.CART_ICON)
 
     def open_first_product(self):
         self.click(*self.ITEM_NAME)
+
