8e8d643b0b5799edc182875f209ad6e7ad457832
Vicky Spandonari <149634185+vickyspandonari@users.noreply.github.com>
2025-12-10 00:06:08 -0300

    Fix: scroll headless para botón Checkout en CI

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -10,4 +10,6 @@ class CartPage(BasePage):
         return len(self.driver.find_elements(*self.CART_ITEM))
 
     def go_to_checkout(self):
+    # Scroll para que el botón quede visible en headless (porque me daba error en github actions)
+        self.driver.execute_script("arguments[0].scrollIntoView();", self.find(*self.CHECKOUT_BTN))
         self.click(*self.CHECKOUT_BTN)
